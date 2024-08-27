b47bd37b4ffdea299030e4338c1463a341b36827
AdmiN <AdmiN@ITWS0023>
2024-08-27 11:46:55 +0530

    cloning

--- src/sampleclass.java
+++ src/sampleclass.java
@@ -3,5 +3,7 @@ public class sampleclass {
 		public static void main(String[] args) {
 			System.out.println("sample class pavan kumar");
 			System.out.println("Main classs");
+			System.out.println("Main classs");
+			System.out.println("Main classs");
 		}
 }
